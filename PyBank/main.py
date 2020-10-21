7554e349d75d78895cbd561d10c4dfab030e74e8
apetek <apetek@apeteks-MBP.fios-router.home>
2020-10-21 14:04:12 -0400

    commit changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,7 +2,16 @@
 import os
 import csv
 
+months = 12
+net_total = 0
+average_changes = 0
+greatest_increase = {}
+greatest_decrease = {}
+
 #Read in a csv file
-budget_data = os.path.join("/Users/apetek/Desktop/Github/python-challange/PyBank/Resources/budget_data (1).csv")
-budget_data
+budget_data = os.path.join("Resources", "budget_data.csv")
+with open(budget_data) as financial_data:
+    reader = csv.reader(financial_data)
+    header = next(reader)
+    print(header)
 
