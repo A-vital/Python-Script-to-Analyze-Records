d98e186482e26f6cce9973a3d7ba7eb6b34bdfee
apetek <apetek@apeteks-MBP.fios-router.home>
2020-10-20 20:23:37 -0400

    initial upload

---
+++ PyBank/main.py
@@ -0,0 +1,8 @@
+#Import the necessary dependencies
+import os
+import csv
+
+#Read in a csv file
+budget_data = os.path.join("/Users/apetek/Desktop/Github/python-challange/PyBank/Resources/budget_data (1).csv")
+budget_data
+
