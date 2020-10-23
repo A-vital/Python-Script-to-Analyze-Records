730191af817cb29a503bc64ab7e746339b21b677
apetek <apetek@apeteks-MBP.fios-router.home>
2020-10-22 22:51:21 -0400

    update

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,9 @@
 #Import the necessary dependencies
 import os
 import csv
+from csv import reader
 
-months = 12
+months = 0
 net_total = 0
 average_changes = 0
 greatest_increase = {}
@@ -11,7 +12,15 @@ greatest_decrease = {}
 #Read in a csv file
 budget_data = os.path.join("Resources", "budget_data.csv")
 with open(budget_data) as financial_data:
-    reader = csv.reader(financial_data)
-    header = next(reader)
-    print(header)
+    csv_reader = reader(financial_data)
+    #reader = csv.reader(financial_data)
+    #header = next(reader)
+    #firstrow = next(reader)
+    #months = months + 1
+    #net_total = net
+    for row in csv_reader:
+        months += 1
+        print(months)
+        #net_total = net_total + header[1]
+        #averge_changes = (net_total/months)
 
