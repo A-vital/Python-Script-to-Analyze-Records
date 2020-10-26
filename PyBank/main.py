9d2e366b2adeb4065d741f6a5ba13cb6cdfb6329
apetek <apetek@apeteks-MBP.fios-router.home>
2020-10-25 21:37:26 -0400

    update

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,12 +14,12 @@ budget_data = os.path.join("Resources", "budget_data.csv")
 with open(budget_data) as financial_data:
     csv_reader = reader(financial_data)
     #reader = csv.reader(financial_data)
-    #header = next(reader)
-    #firstrow = next(reader)
-    #months = months + 1
-    #net_total = net
+    header = next(csv_reader)
+    firstrow = next(csv_reader)
+    months = months + 1
+    net_total = net_total + net_total
     for row in csv_reader:
-        months += 1
+        months = sum(1 for row in csv_reader) + 2
         print(months)
         #net_total = net_total + header[1]
         #averge_changes = (net_total/months)
