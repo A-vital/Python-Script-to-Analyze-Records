8d17e533c856b7e38ca5c4078a1107dd861e74ed
apetek <apetek@apeteks-MBP.fios-router.home>
2021-01-10 08:48:07 -0500

    python

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,10 +1,12 @@
 import os
 import csv
 from csv import reader
+from operator import itemgetter
 
 votes = 0
 winner_votes = 0
 total_candidates = 0
+candidates = []
 candidate_votes = {}
 
 election = os.path.join("Resources", "election_data.csv")
@@ -15,11 +17,20 @@ with open(election, newline="") as csvfile:
         votes = votes + 1
         total_candidates = row["Candidate"]        
 
-        if row["Candidate"] not in candidate_options:
+        if row["Candidate"] not in candidates:
             
-            candidate_options.append(row["Candidate"])
+            candidates.append(row["Candidate"])
 
             candidate_votes[row["Candidate"]] = 1
             
         else:
             candidate_votes[row["Candidate"]] = candidate_votes[row["Candidate"]] + 1
+
+    print("Election Results")
+    print("-------------------------")
+    print("Total Votes " + str(votes))
+    print("-------------------------")
+#results
+    for candidate in candidate_votes:
+        print(candidate + " " + str(round(((candidate_votes[candidate]/votes)*100))) + "%" + " (" + str(candidate_votes[candidate]) + ")") 
+        candidate_results = (candidate + " " + str(round(((candidate_votes[candidate]/votes)*100))) + "%" + " (" + str(candidate_votes[candidate]) + ")") 
