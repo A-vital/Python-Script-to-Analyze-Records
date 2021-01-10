0c6cc27417b1efafedb291942f899b6d4cdfe00a
apetek <apetek@apeteks-MBP.fios-router.home>
2021-01-10 08:36:26 -0500

    python

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,4 +8,18 @@ total_candidates = 0
 candidate_votes = {}
 
 election = os.path.join("Resources", "election_data.csv")
-with open(election, newline="") as csvfile
+with open(election, newline="") as csvfile:
+    reader = csv.reader(csvfile)
+
+    for row in reader:
+        votes = votes + 1
+        total_candidates = row["Candidate"]        
+
+        if row["Candidate"] not in candidate_options:
+            
+            candidate_options.append(row["Candidate"])
+
+            candidate_votes[row["Candidate"]] = 1
+            
+        else:
+            candidate_votes[row["Candidate"]] = candidate_votes[row["Candidate"]] + 1
